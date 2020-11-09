fbd59e7968039ef0f321ee79d1c59bf448bf7ceb
Lhoussaine <bouaik.lhou@gmail.com>
2020-11-09 10:52:52 +0100

    Make test for appointement model

---
+++ spec/models/appointement_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe Appointement, type: :model do
+
+  describe 'Association' do
+
+    it { should belong_to(:car) }
+    it { should belong_to(:user) }
+    
+  end
+end
